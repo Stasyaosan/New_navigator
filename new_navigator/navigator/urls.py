172d1de1fe33a8c08272c2368bd99185ebcf0643
Stasyaosan <smavrin38@gmail.com>
2024-11-05 17:58:04 +0300

    написаны модели и начата работа с fullcalendar

--- new_navigator/navigator/urls.py
+++ new_navigator/navigator/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('api/schedule/', ScheduleListView.as_view(), name='schedule-list')
 ]
--- new_navigator/navigator/views.py
+++ new_navigator/navigator/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+from django.views import View
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class ScheduleListView(View):
+    def get(self, request):
+        schedules = Schedule.objects.all()
+        events = []
+        for schedule in schedules:
+            event = {
+                'title': schedule.lesson.name + "(" + schedules.teacher.name + ")",
+                'start': schedule.day_of_week + "T" + schedule.start_time,
+                'end': schedule.day_of_week + "T" + schedule.end_time,
+            }
+            events.append(event)
+        return JsonResponse(events, safe=False)

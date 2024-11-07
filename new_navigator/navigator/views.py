cc3f711b1c8d9960cfdcc9430d86ec3142a6aa4d
Stasyaosan <smavrin38@gmail.com>
2024-11-07 19:56:22 +0300

    fulcaledar работает

--- new_navigator/navigator/views.py
+++ new_navigator/navigator/views.py
@@ -12,11 +12,27 @@ class ScheduleListView(View):
     def get(self, request):
         schedules = Schedule.objects.all()
         events = []
+
+        #schedule.day_of_week+"T"
         for schedule in schedules:
+            print(str(schedule.start_time)[:5])
+            day = 0
+            if schedule.day_of_week == 'Monday':
+                day = 1
+            elif schedule.day_of_week == 'Tuesday':
+                day = 2
+            elif schedule.day_of_week == 'Wednesday':
+                day = 3
+            elif schedule.day_of_week == 'Thursday':
+                day = 4
+            elif schedule.day_of_week == 'Friday':
+                day = 5
             event = {
-                'title': schedule.lesson.name + "(" + schedules.teacher.name + ")",
-                'start': schedule.day_of_week + "T" + schedule.start_time,
-                'end': schedule.day_of_week + "T" + schedule.end_time,
+                'title': schedule.lesson.name + "(" + schedule.teacher.name + ")",
+                'daysOfWeek': f'[{day}]',
+                'startTime': str(schedule.start_time)[:5],
+                'endTime': str(schedule.end_time)[:5],
+                'backgroundColor': '#000'
             }
             events.append(event)
         return JsonResponse(events, safe=False)
